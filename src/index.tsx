e4ebff8de17e96818bbfb6fcaba2a1087115cf47
Neakhai Yauheni <beonezero@protonmail.com>
2024-02-03 12:28:39 +0300

    в

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import reportWebVitals from "./reportWebVitals"
-import { App } from "./App/App"
+import { App } from "App/App"
 import { Provider } from "react-redux"
-import { store } from "./App/store"
+import { store } from "App/store"
 import { BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
