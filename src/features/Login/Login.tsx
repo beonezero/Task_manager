37fad3cd4af0720986b5ee067fb0263fbc80594d
Neakhai Yauheni <beonezero@protonmail.com>
2024-02-15 23:13:49 +0300

    refactoring Login.tsx

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -22,18 +22,18 @@ export const Login = React.memo(() => {
       rememberMe: false,
     },
     validate: (values) => {
-      const errors: FormikErrorType = {}
-      if (!values.email) {
-        errors.email = "Required"
-      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = "Invalid email address"
-      }
-      if (!values.password) {
-        errors.password = "Required"
-      } else if (values.password.length < 5) {
-        errors.password = "Most be more 5 symbols"
-      }
-      return errors
+      // const errors: FormikErrorType = {}
+      // if (!values.email) {
+      //   errors.email = "Required"
+      // } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      //   errors.email = "Invalid email address"
+      // }
+      // if (!values.password) {
+      //   errors.password = "Required"
+      // } else if (values.password.length < 5) {
+      //   errors.password = "Most be more 5 symbols"
+      // }
+      // return errors
     },
     onSubmit: (data) => {
       dispatch(authThunks.login(data))
